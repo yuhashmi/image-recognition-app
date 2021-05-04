59119e89ded13be1a0fede2c772121a6ffae1dd9
yuhashmi <giogiosef97@gmail.com>
2021-05-04 13:51:15 +0500

    making the input and submit functional

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
 import './App.css';
+import React from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+import { render } from '@testing-library/react';
 
-function App() {
+const particleOptions = { 
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800,
+      }
+    }
+  }
+}
+
+class App extends React.Component { 
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
+  render() {
   return (
     <div className="App">
+      <Particles className="particles" 
+        params={particleOptions}
+      />
       <Navigation />
       <Logo />
-      <ImageLinkForm />
+      <Rank />
+      <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit/>
       {/* <FaceRecognition /> */}
     </div>
   );
+}
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange }) => {
     return (
         <div>
             <p className='f3'>
                 {'This will detect faces in your pictures'}
             </p>
-            <div>
-                <input className='f4 pa2 w-70 center' type="text" />
-                <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">Detect</button>
+            <div className="center">
+                <div className="form center pa4 br3 shadow-5">
+                    <input className='f4 pa2 w-70 light-silver bg-near-black center' type="text" onChange={onInputChange} />
+                    <button className="w-30 grow f4 link ph3 pv2 dib pink bg-dark-gray">Detect</button>
+                </div>
             </div>
         </div>
     );
